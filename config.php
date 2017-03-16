116b91fe94e9226fc6056f76df71cb3e38b699da
Proyecto CCred <samu.br0996@gmail.com>
2017-03-16 16:48:16 -0600

    -s sc2

--- config.php
+++ config.php
@@ -6,12 +6,3 @@
 
     $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 ?>
-
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', '');
-   define('DB_DATABASE', 'ccred_conform');
-
-    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
-?>
