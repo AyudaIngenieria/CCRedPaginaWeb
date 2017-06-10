9a2a78119c496da0e221633bc9b8858706125d8e
Samuel <samu.br0996@gmail.com>
2017-06-09 19:34:35 -0500

    -s cambio buscar

--- tienda.php
+++ tienda.php
@@ -106,78 +106,13 @@
         </div>
       </nav>
 
-      <form id="form1" name="form1" method="post" action="">
-      </form>
-      <table width="958" border="1" align="center">
-        <tr>
-          <td colspan="5">&nbsp;</td>
-          <td width="65">BUSCAR:</td>
-          <td width="163"><form id="form2" name="form2" method="post" action="">
-            <label for="buscar"></label>
-            <input type="text" name="buscar" id="buscar" />
-          </form></td>
-          <td width="105"><form id="form3" name="form3" method="post" action="">
-            <form method="post" type="submit">
-            <input type="submit" name="Aceptar" id="Aceptar" value="Aceptar" />
-            </form>
-          </form></td>
-        </tr>
-        <tr>
-          <td colspan="8" align="center" bgcolor="#999999">LISTADO DE PRODUCTOS</td>
-        </tr>
-        <tr>
-          <td width="43" align="center" bgcolor="#FF6600"> ID   </td>
-          <td width="124" align="center" bgcolor="#FF6600">IMAGEN</td>
-          <td width="94" align="center" bgcolor="#FF6600">NOMBRE</td>
-          <td width="240" align="center" bgcolor="#FF6600">DESCRIPCIÓN</td>
-          <td width="72" align="center" bgcolor="#FF6600">PRECIO</td>
-          <td align="center" bgcolor="#FF6600">EN STOCK</td>
-          <td align="center" bgcolor="#FF6600">FECHA</td>
-          <td align="center" bgcolor="#FF6600">AGREGAR </td>
-        </tr>
-
-        <?php
-        $consulta=mysql_query("select * from productos");
-          if (isset($_POST['buscar'])){
-        	  $consulta=mysql_query("select * from productos where nombre like '%" .$_POST['buscar']. "%'");
-          }
-
-        	while($filas=mysql_fetch_array($consulta)){
-      		$id=$filas['id'];
-        	$imagen=$filas['imagen'];
-        	$nombre=$filas['nombre'];
-        	$desc=$filas['descripcion'];
-      		$precio=$filas['precio'];
-      		$enStock=$filas['cuanto_hay'];
-      		$fecha=$filas['fecha'];
-
-        ?>
-
-        <tr>
-          <td align="center" valign="middle"><?php echo $id ?></td>
-          <td align="center" valign="middle"><img src="<?php echo $imagen; ?>" width="195" height="130" /><br /></td>
-          <td align="center" valign="middle"><?php echo $nombre ?></td>
-          <td align="center" valign="middle"><?php echo $desc ?></td>
-          <td align="center" valign="middle"><?php echo $precio ?></td>
-          <td align="center" valign="middle"><?php echo $enStock ?></td>
-          <td align="center" valign="middle"><?php echo $fecha ?></td>
-          <td align="center" valign="middle">
-            <form action="carrito.php" method="post" name="compra">
-              <input name="id_txt" type="hidden" value="<?php echo $id ?>" />
-              <input name="nombre" type="hidden" value="<?php echo $nombre ?>" />
-              <input name="precio" type="hidden" value="<?php echo $precio ?>" />
-              <input name="cantidad" type="hidden" value="1" />
-              <input name="Comprar" type="submit" value="Comprar" />
-            </form>
-          </td>
-        </tr>
-
-         <?php } ?>
-      </table>
+<?php $a=verListadoProductos ('comprar') ?>
 
 <br>
 <br>
 <br>
+
+<p><a href="carrito.php">Ver carrito</a></p>
 
       <footer class="footer-distributed">
 
