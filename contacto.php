edfafa6769965f77aec15d6f18b38e20f6cd1633
Samuel <samu.br0996@gmail.com>
2017-07-12 00:36:37 -0500

    -s index

--- contacto.php
+++ contacto.php
@@ -21,7 +21,9 @@
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0,
+    maximum-scale=1.0, minimum-scale=1.0">
+
     <meta name="keywords" content="footer, links, icons" />
 
     <link rel="stylesheet" href="css/demo.css">
@@ -41,7 +43,7 @@
           <div class="col-sm-2">
             <ul class="nav navbar-nav">
                 <a href="index.html">
-                  <img src="img/LogosCCRED/logoblanco.png"  style="margin-top;" width="55px" height="50px">
+                  <img src="img/LogosCCRED/logoccred.png"  style="margin-top;" width="55px" height="50px">
                 </a>
             </ul>
           </div>
